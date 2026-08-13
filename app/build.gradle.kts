d3d34096545a0e96fdfa7208b662844ed6b63d50
elargantiny16 <elargantiny16@gmail.com>
2026-08-13 23:43:42 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.bluethread.app"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.bluethread.app"
         minSdk = 26
         targetSdk = 35
         versionCode = 4
         versionName = "1.3"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
     }
 }
